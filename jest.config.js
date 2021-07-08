4566fe3e5d62dd307b1645a5f56ae854ccbfde49
georginarojas <ginarojas29@gmail.com>
2021-07-08 12:01:08 -0300

    refactor: change the extension ts for js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-export default {
+module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
@@ -19,9 +19,9 @@ export default {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.ts?$': 'ts-jest'
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  collectCoverage: true,
-  clearMocks: true
+  }
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  // moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  // collectCoverage: true,
+  // clearMocks: true
 }
