61ba5fd6644676b67590190924842b45fe956e23
georginarojas <ginarojas29@gmail.com>
2021-07-09 19:54:03 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -3,6 +3,10 @@ import { AccountMongoRepository } from './account'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
 describe('Account Mongo Repository', () => {
   // Connect and disconnect the mongodb
   beforeAll(async () => {
@@ -15,7 +19,7 @@ describe('Account Mongo Repository', () => {
   })
   // -----------------------------------
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
